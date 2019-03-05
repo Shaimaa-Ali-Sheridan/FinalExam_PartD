98a72b859b258842ef84fb2d500ce793593f4cd8
Shaimaa-Ali-Sheridan <shaimaa.ali1@sheridancollege.ca>
2019-03-05 00:03:53 -0500

    Initial Commit

---
+++ src/atmsystem/ATM.java
@@ -0,0 +1,86 @@
+package atmsystem;
+
+/**
+ *
+ * @author shaim
+ */
+public class ATM {
+    final static String[] ACCOUNT_TYPES = {"Debit", "Credit", "Saving"};
+   private long accountNumber;
+   private String accountUserName;
+   private String accountPassword;
+   private double accountBalance;
+   private String accountType;
+   
+    final static String[] ATM_OPTIONS = {"login","deposit","withdraw"};
+
+    /**
+     * @return the accountNumber
+     */
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * @param accountNumber the accountNumber to set
+     */
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * @return the accountUserName
+     */
+    public String getAccountUserName() {
+        return accountUserName;
+    }
+
+    /**
+     * @param accountUserName the accountUserName to set
+     */
+    public void setAccountUserName(String accountUserName) {
+        this.accountUserName = accountUserName;
+    }
+
+    /**
+     * @return the accountPassword
+     */
+    public String getAccountPassword() {
+        return accountPassword;
+    }
+
+    /**
+     * @param accountPassword the accountPassword to set
+     */
+    public void setAccountPassword(String accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+
+    /**
+     * @return the accountBalance
+     */
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    /**
+     * @param accountBalance the accountBalance to set
+     */
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    /**
+     * @return the accountType
+     */
+    public String getAccountType() {
+        return accountType;
+    }
+
+    /**
+     * @param accountType the accountType to set
+     */
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+}
---
+++ src/atmsystem/ATMSystem.java
@@ -0,0 +1,64 @@
+package atmsystem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shaim
+ */
+public class ATMSystem {
+
+   
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ATM atm = new ATM();
+         //Display available options 
+         System.out.println("Please choose the option number:");
+         for (int i=0; i<ATM.ATM_OPTIONS.length; i++)
+             System.out.println(i +" - " + ATM.ATM_OPTIONS[i]);
+         
+         Scanner sc = new Scanner (System.in);
+         int option = sc.nextInt();
+         if (option == 0)
+         {
+             System.out.println("Please enter user name:");
+             String userName = sc.nextLine();
+             atm.setAccountUserName(userName);
+             
+             System.out.println("Please enter user name:");
+             String password = sc.nextLine();
+             atm.setAccountUserName(userName);
+         }
+         else
+         {
+             if (option == 1)
+             {
+                 System.out.println("Please enter amount to deposit");
+                 double newAmount = sc.nextDouble();
+                 atm.setAccountBalance(atm.getAccountBalance() + newAmount);
+                 System.out.println("New balance is:" + atm.getAccountBalance());
+             }
+             else
+             {
+                 if (option == 2)
+                 {
+                     System.out.println("Please enter amount to withdraw:");
+                    double amount = sc.nextDouble();
+                 
+                    // make sure there's enough balance before withdrawal
+                    double currentBalance = atm.getAccountBalance();
+                    double balanceAfterWithdraw = currentBalance - amount;
+                    if ( balanceAfterWithdraw > 0 )
+                      atm.setAccountBalance(balanceAfterWithdraw);
+                    else
+                        System.out.println("Balance is not sufficient");
+                 }
+             }
+         }
+    }
+    
+}
